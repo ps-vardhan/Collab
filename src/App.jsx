9dcb8ca36ca9a83eeb1cf0387255755c0131caf8
Vardhan Reddy <peesarisaivardhan@gmail.com>
2026-03-12 17:44:37 +0530

    Removed dead  login code for roommanager

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import EditorPage from "./components/EditorPage";
 import LoginPage from "./components/LoginPage";
-// import RoomManager from "./components/RoomManager";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-
-        <Route path="/login" element={<LoginPage />} />
-
-        {/* <Route path="/dashboard" element={<RoomManager />} /> */}
 
         <Route path="/editor/:roomId" element={<EditorPage />} />
       </Routes>
