fec402298ae0b21f2975509b86d28855f69a988a
Vardhan Reddy <peesarisaivardhan@gmail.com>
2026-03-12 19:15:03 +0530

    Added Protected route for safe transit between login and room creation.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import "./App.css";
 import EditorPage from "./components/EditorPage";
 import LoginPage from "./components/LoginPage";
 
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/" replace />;
+};
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
 
-        <Route path="/editor/:roomId" element={<EditorPage />} />
+        <Route
+          path="/editor/:roomId"
+          element={
+            <ProtectedRoute>
+              <EditorPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
