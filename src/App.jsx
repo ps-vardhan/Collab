a8c076c0d8fc3d94a270317d5bd52e711c05feab
Vardhan Reddy <peesarisaivardhan@gmail.com>
2026-03-13 23:22:54 +0530

    add WhiteboardPage route and implement ProtectedRoute for session security

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import EditorPage from "./components/EditorPage";
 import LoginPage from "./components/LoginPage";
+import WhiteboardPage from "./components/WhiteboardPage";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
@@ -19,6 +21,15 @@ function App() {
           element={
             <ProtectedRoute>
               <EditorPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/whiteboard/:roomId"
+          element={
+            <ProtectedRoute>
+              <WhiteboardPage />
             </ProtectedRoute>
           }
         />
